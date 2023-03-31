2a1dbd36bb176ec586854ed4b492f4786cbce6d4
BrayanMaximiano <brayanmaximiano01@gmail.com>
2023-03-31 13:36:35 -0300

    finalização pagina Home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,16 @@
-import { Container } from "./styles"
+import { Container, HomeWrapper, Img, ImgWrapper } from "./styles"
 import SideNavigation from "../../components/SideNavigation"
+import NovadirecaoLogo from '../../assets/novadirecaoLogo.png'
 
 const Index = ()=>{
     return(
         <Container>
             <SideNavigation/>
+            <HomeWrapper>
+                <ImgWrapper>
+                    <Img src={NovadirecaoLogo}/>
+                </ImgWrapper>
+            </HomeWrapper>
         </Container>
     )
 }
--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -4,4 +4,22 @@ import styled from "styled-components";
 export const Container = styled.div`
  width: 100vw;
  height: 100vh;
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
+`
+export const HomeWrapper = styled.div`
+    width: 857px;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const ImgWrapper = styled.div`
+ width: 450px;
+ height: 450px;
+`
+export const Img = styled.img`
+    width: 100%;
+    height: 100%;
 `
