c6ceac04f87ef9e0a23fc7ed728e2feae6a044b8
BrayanMaximiano <brayanmaximiano01@gmail.com>
2023-05-15 11:49:09 -0300

    Inicio Area de contrato

--- src/Pages/Contrato/index.jsx
+++ src/Pages/Contrato/index.jsx
@@ -1,11 +1,22 @@
-import { Container, ContratoWrapper } from "./styles";
+import { Box1, CidadeNascimento, Container, ContratoWrapper, EstadoCivil, Label, Nascimento, Nome, Sexo, UF } from "./styles";
 import SideNavigation from "../../components/SideNavigation";
 
 const Index = () => {
   return (
     <Container>
       <SideNavigation/>
-      <ContratoWrapper></ContratoWrapper>
+      <ContratoWrapper>
+        <Box1>
+          
+          <Nome placeholder="Nome"/>
+          <Nascimento placeholder="Nascimento"/>
+          <CidadeNascimento placeholder="Cidade de Nascimento"/>
+          <UF placeholder="UF"/>
+          <Sexo placeholder="Sexo"/>
+          <EstadoCivil placeholder="Estado Civil"/>
+          
+        </Box1>
+      </ContratoWrapper>
     </Container>
   );
 };
--- src/Pages/Contrato/styles.js
+++ src/Pages/Contrato/styles.js
@@ -16,6 +16,69 @@ export const ContratoWrapper = styled.div`
     border-radius: 10px;
 `
 
-export const ContratoInput = styled.input`
+export const Box1 = styled.div`
+width: 100%;
+height: 25%;
+background-color: red;
+padding:20px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-wrap: wrap;
 
+
+`
+
+export const Nome = styled.input`
+  background-color:white;
+  height:40px;
+  width: 475px;
+  border-radius:10px;
+  border:none;
+  text-align: center;
+`
+export const Nascimento = styled.input`
+ background-color:white;
+ height:40px;
+ width: 208px;
+ border-radius:10px;
+ border:none;
+`
+export const CidadeNascimento = styled.input`
+ background-color:white;
+ height:40px;
+ width: 244px;
+ border-radius:10px;
+ border:none;
+`
+export const UF = styled.input`
+ background-color:white;
+ height:40px;
+ width: 57px;
+ border-radius:10px;
+ border:none;
+`
+export const Sexo = styled.input`
+ background-color:white;
+ height:40px;
+ width: 75px;
+ border-radius:10px;
+ border:none;
+ `
+export const EstadoCivil = styled.input`
+ background-color:white;
+ height:40px;
+ width: 208px;
+ border-radius:10px;
+ border:none;
+`
+export const Box2 = styled.div`
+width: 100%;
+height: 25%;
+background-color: green;
+padding:20px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+flex-wrap: wrap;
 `
