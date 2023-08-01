acc90b6c5f3607b798418bfdb0fa07e0d5079ecd
BrayanMaximiano <brayanmaximiano01@gmail.com>
2023-08-01 11:22:41 -0300

    Armazenamento dos Inputs

--- src/Pages/Contrato/index.jsx
+++ src/Pages/Contrato/index.jsx
@@ -33,51 +33,183 @@ import {
   UfRg,
 } from "./styles";
 import SideNavigation from "../../components/SideNavigation";
+import { useState } from "react";
 
 const Index = () => {
 
+ 
+
+ const [values,setValues] = useState();
+
+ const HandleChangeValues = (value) => {
+  setValues((prevValue)=>({
+    ...prevValue,
+    [value.target.name]: value.target.value,
+  }));
+ }
+
  function ImprimeContrato(){
-  window.alert('Imprimindo contrato')
+  console.log(values)
  };
  function ImprimeConferencia(){
   window.alert('Imprimindo conferencia')
  };
+
+
   return (
     <Container>
       <SideNavigation />
       <ContratoWrapper>
         <Box1>
-          <Nome placeholder="Nome" />
-          <Nascimento placeholder="Nascimento" />
-          <CidadeNascimento placeholder="Cidade de Nascimento" />
-          <UF placeholder="UF" />
-          <Sexo placeholder="Sexo" />
-          <EstadoCivil placeholder="Estado Civil" />
+          <Nome 
+          placeholder="Nome"
+          type="text"
+          name="Nome"
+          onChange={HandleChangeValues} />
+
+          <Nascimento 
+          placeholder="Nascimento"
+          type="text"
+          name="Nascimento"
+          onChange={HandleChangeValues} />
+
+          <CidadeNascimento 
+          placeholder="Cidade de Nascimento"
+          type="text"
+          name="Cidade"
+          onChange={HandleChangeValues} />
+
+          <UF 
+          placeholder="UF"
+          type="text"
+          name="Uf"
+          onChange={HandleChangeValues} />
+
+          <Sexo 
+          placeholder="Sexo"
+          type="text"
+          name="Sexo" 
+          onChange={HandleChangeValues}/>
+
+          <EstadoCivil 
+          placeholder="Estado Civil"
+          type="text"
+          name="EstadoCivil"
+          onChange={HandleChangeValues} />
         </Box1>
 
         <Box2>
-          <EnderecoResidencial placeholder="Endereço Residencial" />
-          <Numero placeholder="Número" />
-          <Complemento placeholder="Complemento" />
-          <BairroResidencial placeholder="Bairro Residencial" />
-          <CidadeResidencial placeholder="Cidade Residencial" />
-          <Uf placeholder="UF" />
-          <Cep placeholder="CEP" />
+          <EnderecoResidencial 
+          placeholder="Endereço Residencial"
+          type="text"
+          name="EnderecoResidencial" 
+          onChange={HandleChangeValues}/>
+
+          <Numero 
+          placeholder="Número" 
+          type="text"
+          name="NumeroResidencial"
+          onChange={HandleChangeValues}/>
+
+          <Complemento 
+          placeholder="Complemento"
+          type="text"
+          name="Complemento"
+          onChange={HandleChangeValues} />
+
+          <BairroResidencial 
+          placeholder="Bairro Residencial"
+          type="text"
+          name="BairroResidencial"
+          onChange={HandleChangeValues} />
+
+          <CidadeResidencial 
+          placeholder="Cidade Residencial"
+          name="CidadeResidencial"
+          type="text"
+          onChange={HandleChangeValues} />
+
+          <Uf 
+          placeholder="UF" 
+          type="text"
+          onChange={HandleChangeValues}
+          name="UfNascimento"/>
+
+          <Cep placeholder="CEP" 
+          type="text"
+          onChange={HandleChangeValues}
+          name="CEP"/>
+          
         </Box2>
         <Box3>
-          <Cpts placeholder="C.P.T.S." />
-          <Serie placeholder="Série" />
-          <UfCpts placeholder="UF" />
-          <DtCpts placeholder="Emissão" />
-          <Rg placeholder="RG" />
-          <UfRg placeholder="UF" />
-          <DtRg placeholder="Emissão" />
-          <CPF placeholder="C.P.F." />
-          <Pis placeholder="P.I.S." />
+          <Cpts 
+          placeholder="C.P.T.S."
+          type="text"
+          name="Cpts"
+          onChange={HandleChangeValues} />
+
+          <Serie 
+          placeholder="Série"
+          type="text"
+          name="Serie"
+          onChange={HandleChangeValues} />
+
+          <UfCpts 
+          placeholder="UF"
+          type="text"
+          name="UfCpts"
+          onChange={HandleChangeValues} />
+
+          <DtCpts 
+          placeholder="Emissão"
+          type="text"
+          name="DataCpts"
+          onChange={HandleChangeValues} />
+
+          <Rg 
+          placeholder="RG"
+          type="text"
+          name="RG"
+          onChange={HandleChangeValues} />
+
+          <UfRg 
+          placeholder="UF"
+          type="text"
+          name="UfRg"
+          onChange={HandleChangeValues} />
+
+          <DtRg 
+          placeholder="Emissão"
+          type="text"
+          name="DataRg"
+          onChange={HandleChangeValues} />
+
+          <CPF 
+          placeholder="C.P.F."
+          type="text"
+          name="Cpf"
+          onChange={HandleChangeValues} />
+
+          <Pis 
+          placeholder="P.I.S."
+          type="text"
+          name="Pis"
+          onChange={HandleChangeValues} />
+
         </Box3>
         <Box4>
-          <NomeMae placeholder="Nome da Mãe" />
-          <NomePai placeholder="Nome do Pai" />
+          <NomeMae 
+          placeholder="Nome da Mãe"
+          name="NomeDaMae"
+          type="text"
+          onChange={HandleChangeValues} />
+
+          <NomePai 
+          placeholder="Nome do Pai"
+          name="NomeDoPai"
+          type="text"
+          onChange={HandleChangeValues} />
+
         </Box4>
         <Box5>
           <Button onClick={ImprimeContrato}>Imprimir Contrato</Button>
